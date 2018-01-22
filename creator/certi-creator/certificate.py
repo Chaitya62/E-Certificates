cb22a37edf701c5380d4acfe84f4994827472711
Chaitya62 <Chaitya62@gmail.com>
2018-01-22 11:32:41 +0530

    Basic Certificate creation functionality

--- creator/certi-creator/certificate.py
+++ creator/certi-creator/certificate.py
@@ -32,9 +32,12 @@ class Certificate():
 	def load_certificate(self):
 		self.certificate = Image.open(self.path)
 
-	def load_font(self, font_path. font_size):
+	def load_font(self, font_path, font_size):
 		font = ImageFont.truetype(font_path, font_size)
 		self.font = font
+
+	def put_image(self, img, x, y):
+		self.certificate.paste(img, (x, y))
 
 	def write(self, x, y, text):
 		# load font first
@@ -42,8 +45,26 @@ class Certificate():
 		draw_certificate.text((x, y),text, self.fontColor, font=self.font)
 
 
+
+
+
 	def save(self):
 		self.certificate.save('out.png')
+
+
+
+cer = Certificate('Chaitya Shah', 'Intro to CP', 'null')
+
+cer.certificate = Image.new('RGBA',(800, 400) ,(255,255,255,255))
+
+
+cer.load_font('font.ttf', 100)
+img = Image.new('RGBA', (100, 50), (255,255,2, 255))
+cer.write(100, 150, 'Chaitya Shah')
+cer.put_image(img, 200, 300)
+cer.put_image(img, 500, 300)
+cer.save()
+
 
 	
 
